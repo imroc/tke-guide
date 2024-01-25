3a00e3ddcbd55f1a74099d0c022c4542347ecb14
roc <roc@imroc.cc>
2024-01-25 20:05:44 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# TKE 实践指南
+
+本书将介绍 TKE 相关实战经验与总结。
+
+## 关于本书
+
+本书为电子书形式，内容为本人多年的腾讯云 TKE 实战经验和笔记。
+
+## 在线阅读
+
+地址：https://imroc.cc/tke
+
+## 评论与互动
+
+本书已集成 [giscus](https://giscus.app/zh-CN) 评论系统，欢迎对感兴趣的文章进行评论与交流。
+
+## 贡献
+
+本书使用 [docusaurus](https://docusaurus.io/) 构建，已集成自动构建和发布，欢迎 Fork 并 PR 来贡献干货内容 (点击左下角 `编辑此页` 按钮可快速修改文章)。
+
+## 许可证
+
+您可以使用 [署名 - 非商业性使用 - 相同方式共享 4.0 (CC BY-NC-SA 4.0)](https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh) 协议共享。
---
+++ content/sidebars.ts
@@ -0,0 +1,153 @@
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+  tkeSidebar: [
+    {
+      type: "doc",
+      id: "README",
+      customProps: {
+        slug: "/"
+      }
+    },
+    {
+      type: 'category',
+      label: 'Serverless 集群与超级节点',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/serverless'
+      },
+      items: [
+        'serverless/precautions',
+        'serverless/why-tke-supernode-rocks',
+        'serverless/supernode-case-online',
+        'serverless/supernode-case-offline',
+        'serverless/large-image-solution',
+      ],
+    },
+    {
+      type: 'category',
+      label: '网络指南',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/networking'
+      },
+      items: [
+        'networking/clb-to-pod-directly',
+        'networking/how-to-use-eip',
+        'networking/install-localdns-with-ipvs',
+        'networking/expose-grpc-with-tcm',
+      ],
+    },
+    {
+      type: 'category',
+      label: '存储指南',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/storage'
+      },
+      items: [
+        'storage/cbs-pvc-expansion',
+        'storage/readonlymany-pv',
+        'storage/mount-cfs-with-v3',
+      ],
+    },
+    {
+      type: 'category',
+      label: '监控告警',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/monitoring'
+      },
+      items: [
+        'monitoring/prometheus-scrape-config',
+        'monitoring/grafana-dashboard-for-supernode-pod',
+      ],
+    },
+    {
+      type: 'category',
+      label: '镜像与仓库',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/images'
+      },
+      items: [
+        'images/use-mirror-in-container',
+        'images/use-foreign-container-image',
+      ],
+    },
+    {
+      type: 'category',
+      label: '故障排查',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/troubleshooting'
+      },
+      items: [
+        'troubleshooting/public-service-or-ingress-connect-failed',
+      ],
+    },
+    {
+      type: 'category',
+      label: '常见应用安装与部署',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/install-apps'
+      },
+      items: [
+        'install-apps/install-harbor-on-tke',
+        'install-apps/install-gitlab-on-tke',
+        'install-apps/install-kubesphere-on-tke',
+      ],
+    },
+    {
+      type: 'category',
+      label: '常见问题',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/faq'
+      },
+      items: [
+        'faq/modify-rp-filter-causing-exception',
+        'faq/clb-loopback',
+        'faq/controller-manager-and-scheduler-unhealthy',
+      ],
+    },
+    {
+      type: 'category',
+      label: '解决方案',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/solution'
+      },
+      items: [
+        'solution/multi-account',
+        'solution/upgrade-inplace',
+      ],
+    },
+    {
+      type: 'category',
+      label: '附录',
+      collapsed: true,
+      link: {
+        type: 'generated-index',
+        slug: '/appendix'
+      },
+      items: [
+        'appendix/useful-kubectl-for-tencent-cloud',
+        'appendix/eks-annotations',
+        'appendix/ingress-error-code',
+      ],
+    },
+  ]
+};
+
+export default sidebars;
